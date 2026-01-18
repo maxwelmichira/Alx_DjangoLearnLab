6640e43b8895bfd8188d69c1b1248966310d369b
maxwel michira <maxwelmichira@gmail.com>
2026-01-19 00:03:23 +0300

    Fix views to use render and proper class-based DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
@@ -6,8 +6,7 @@ from .models import Book, Library
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    output = ", ".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output)
+    return render(request, "list_books.html", {"books": books})
 
 
 # Class-based view: library detail
@@ -15,4 +14,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "library_detail.html"
     context_object_name = "library"
-
