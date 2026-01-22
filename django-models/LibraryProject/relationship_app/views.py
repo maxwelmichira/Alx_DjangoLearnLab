24f6ade76dfcbf9c73696d939a47642d8099a9f5
maxwel michira <maxwelmichira@gmail.com>
2026-01-23 02:36:38 +0300

    Separate permission_required import for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library, Author
 from django.views.generic.detail import DetailView
 
