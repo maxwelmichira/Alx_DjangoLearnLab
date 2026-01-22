3d2c436a49d72b155c40ddc7e11941068f3930fb
maxwel michira <maxwelmichira@gmail.com>
2026-01-22 22:23:32 +0300

    Use Book.objects.all() in list_books view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book, Library
 from django.views.generic import DetailView
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
