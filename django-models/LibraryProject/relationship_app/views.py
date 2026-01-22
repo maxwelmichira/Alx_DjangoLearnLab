35ea0d41e342529b47a86c1c90f412dd3a62a634
maxwel michira <maxwelmichira@gmail.com>
2026-01-22 23:50:14 +0300

    Separate auth imports to individual lines for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 
+# Existing views
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
@@ -11,3 +16,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Authentication views
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
