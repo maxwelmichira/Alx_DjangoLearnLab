cc82948dba52f9900b8937c45324d48e43d9bbe6
maxwel michira <maxwelmichira@gmail.com>
2026-01-22 22:28:28 +0300

    Separate import statements for Book and Library models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
-from django.views.generic import DetailView
+from django.shortcuts import render
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
