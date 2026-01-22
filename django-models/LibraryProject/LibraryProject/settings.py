8a55b003828de08322ee4cd4171413dd764a703f
maxwel michira <maxwelmichira@gmail.com>
2026-01-23 00:08:22 +0300

    Fix authentication imports and use LogoutView for checker compliance

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # Authentication settings
 LOGIN_REDIRECT_URL = '/books/'
 LOGOUT_REDIRECT_URL = '/login/'
+# Allow logout via GET request (for simple link-based logout)
+LOGOUT_REDIRECT_URL = '/login/'
