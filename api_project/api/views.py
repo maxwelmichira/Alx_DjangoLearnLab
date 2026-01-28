bd38e4b381340970d346c0b32186dcc557f7c28a
maxwel michira <maxwelmichira@gmail.com>
2026-01-28 23:38:44 +0300

    Add AllowAny permission to BookList view for public access

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AllowAny]  # Allow public access
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
