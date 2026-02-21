02e0cadd3f93334f829f4948ddde293d96248577
maxwel michira <maxwelmichira@gmail.com>
2026-02-21 23:48:40 +0300

    Fix follow/unfollow views to use user_id URL parameter

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -105,14 +105,12 @@ class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
 
     def post(self, request, user_id):
-        user_to_follow = self.get_object()
-
+        user_to_follow = generics.get_object_or_404(CustomUser, pk=user_id)
         if user_to_follow == request.user:
             return Response(
                 {'error': 'You cannot follow yourself.'},
                 status=status.HTTP_400_BAD_REQUEST
             )
-
         request.user.following.add(user_to_follow)
         return Response(
             {'message': f'You are now following {user_to_follow.username}.'},
@@ -125,14 +123,12 @@ class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
 
     def post(self, request, user_id):
-        user_to_unfollow = self.get_object()
-
+        user_to_unfollow = generics.get_object_or_404(CustomUser, pk=user_id)
         if user_to_unfollow == request.user:
             return Response(
                 {'error': 'You cannot unfollow yourself.'},
                 status=status.HTTP_400_BAD_REQUEST
             )
-
         request.user.following.remove(user_to_unfollow)
         return Response(
             {'message': f'You have unfollowed {user_to_unfollow.username}.'},
