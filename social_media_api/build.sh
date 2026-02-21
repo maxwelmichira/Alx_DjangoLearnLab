979a38cefd0ed0bc444ecd6270126e83e64eef84
maxwel michira <maxwelmichira@gmail.com>
2026-02-22 02:15:43 +0300

    Fix production settings module in build and Procfile

--- social_media_api/build.sh
+++ social_media_api/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+DJANGO_SETTINGS_MODULE=social_media_api.settings_production python manage.py collectstatic --no-input
+DJANGO_SETTINGS_MODULE=social_media_api.settings_production python manage.py migrate
