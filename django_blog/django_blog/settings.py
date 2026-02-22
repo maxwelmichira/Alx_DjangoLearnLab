a2942fe6b3b967ebaefff5adf194fe41ddffa1bf
maxwel michira <maxwelmichira@gmail.com>
2026-02-23 00:01:24 +0300

    Add explicit database configuration fields in settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,7 +77,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+}
 }
 
 
