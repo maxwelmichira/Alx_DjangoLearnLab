2fb9ba9f65cdbeccc4b759f35ac52f8e3baedeff
maxwel michira <maxwelmichira@gmail.com>
2026-03-01 01:41:47 +0300

    Fix build.sh and runtime.txt for Render deployment

---
+++ timberflow/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -o errexit
+pip install -r requirements.txt
+DJANGO_SETTINGS_MODULE=timberflow.settings_production python manage.py collectstatic --no-input
+DJANGO_SETTINGS_MODULE=timberflow.settings_production python manage.py migrate
