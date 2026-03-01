761c31bfb0b07c38263eb8321307f35bbcba25bf
maxwel michira <maxwelmichira@gmail.com>
2026-03-01 04:30:20 +0300

    Add seed_demo_data to build script for auto-seeding on deploy

--- timberflow/build.sh
+++ timberflow/build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 DJANGO_SETTINGS_MODULE=timberflow.settings_production python manage.py collectstatic --no-input
 DJANGO_SETTINGS_MODULE=timberflow.settings_production python manage.py migrate
+DJANGO_SETTINGS_MODULE=timberflow.settings_production python manage.py seed_demo_data
